c6fe5ac96750d8234644e4a9e940ea6000a29562
Rishit Shah <aryanrishit1@gmail.com>
2023-08-29 16:57:15 +0530

    Version 1.8.1.1: Only read until stopped

--- content.js
+++ content.js
@@ -1,23 +1,5 @@
-/*
- * Notes: I have decided to remove the "read selected text" functionality
- * in the main branch, only going to work on the "read until spacebar is 
- * pressed" functionality.
-*/
-
 let isReading = false;
 let readingText = '';
-
-// Read Selected Text
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'readSelectedText') {
-    const selectedText = window.getSelection().toString().trim();
-    if (selectedText) {
-      isReading = true;
-      readingText = selectedText;
-      readText();
-    }
-  }
-});
 
 // Read Until Spacebar is Pressed
 function readText() {
