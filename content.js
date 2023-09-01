071d09bfacff6be10e0ba3fea30d25a67f58e0f2
Rishit Shah <aryanrishit1@gmail.com>
2023-09-01 13:30:07 +0530

    Version 1.8.2.2: Tried to fix content.js

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 let isReading = false;
-let readingText = '';
 let utterance = null;
 let fullPageText = '';
 
@@ -11,53 +10,26 @@ document.addEventListener('DOMContentLoaded', () => {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'startReading') {
     if (!isReading) {
-      readingText = fullPageText;
       isReading = true;
-      readText();
-    } 
-    else {
+      readText(fullPageText);
+    } else {
       stopReading();
     }
-  } 
-  else if (message.action === 'pausePlay') {
-    if (isReading) {
-      pausePlay();
-    }
-  } 
+  } else if (message.action === 'pausePlay') {
+    pausePlay();
+  }
 });
 
-function readText() {
-  if (isReading && readingText !== '') {
-    utterance = new SpeechSynthesisUtterance(readingText);
+function readText(text) {
+  if (isReading) {
+    utterance = new SpeechSynthesisUtterance(text);
     speechSynthesis.speak(utterance);
 
     utterance.onend = () => {
       if (isReading) {
-        readNextChunk();
+        stopReading();
       }
     };
-  }
-}
-
-function readNextChunk() {
-  if (isReading && readingText !== '') {
-    const remainingText = readingText.slice(utterance.charIndex);
-    const nextSpaceIndex = remainingText.indexOf(' ');
-
-    if (nextSpaceIndex === -1) {
-      // End of text reached
-      stopReading();
-    } else {
-      const chunk = remainingText.slice(0, nextSpaceIndex + 1);
-      utterance = new SpeechSynthesisUtterance(chunk);
-      speechSynthesis.speak(utterance);
-
-      utterance.onend = () => {
-        if (isReading) {
-          readNextChunk();
-        }
-      };
-    }
   }
 }
 
@@ -67,20 +39,14 @@ function stopReading() {
     speechSynthesis.cancel();
   }
   isReading = false;
-  readingText = '';
 }
 
-// Implement other functionalities like toBeginning, lastWord, and pausePlay
 function pausePlay() {
-  if (isReading && utterance) {
-    if (speechSynthesis.speaking) {
-      if (speechSynthesis.paused) {
-        speechSynthesis.resume();
-      } 
-      else {
-        speechSynthesis.pause();
-      }
+  if (isReading) {
+    if (utterance.paused) {
+      speechSynthesis.resume();
+    } else {
+      speechSynthesis.pause();
     }
   }
 }
-
